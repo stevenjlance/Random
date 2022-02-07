a59014174a8867c044babfee3af133a176444947
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2022-02-07 20:18:07 +0000

    Updated for semester 2

--- script.js
+++ script.js
@@ -2,20 +2,23 @@
 
 
 
-
 function setup() {
   createCanvas(500, 500);
   background(200);
+	// This will slow the rate of the draw funciton so we can see the images that are being drawn.
+	frameRate(5);
+
+
+	
+	
 }
 
 function draw() {
-	// This will slow the rate of the draw funciton so we can see the images that are being drawn.
-	frameRate(5);
 	// Write code below this line
+	
 
 
-	
-	
+
 
 
 
